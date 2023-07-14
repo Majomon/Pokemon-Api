d4c2cd325fba326dc01defe7a8a11a825c6d8f38
Majomon <mauri.monzon91@gmail.com>
2023-07-14 16:17:49 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const cors = require("cors");
 
 require("./db.js");
 
@@ -12,7 +11,6 @@ const server = express();
 
 server.name = "API";
 
-server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
