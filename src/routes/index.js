164c486a2710b2b68b1f02b4b4ff6acbad00ad17
Majomon <mauri.monzon91@gmail.com>
2023-07-14 16:25:36 -0300

    123

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,12 @@ const { URL_BASE } = process.env;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
+const dataAPI = (await axios.get(`${URL_BASE}/1`)).data;
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/", (req, res) => {
-  res.send("Holis. Estas en la home de la Api de mauri :)" + URL_BASE);
+  res.send("Holis. Estas en la home de la Api de mauri :)" + dataAPI);
 });
 
 //* Defino los dos routers, pokemons y types
