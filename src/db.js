72183f95457177f5251d3078104201b30088106c
Majomon <mauri.monzon91@gmail.com>
2023-07-14 13:09:34 -0300

    act

--- src/db.js
+++ src/db.js
@@ -6,20 +6,19 @@ const { DB_USER, DB_PASSWORD, DB_HOST, PGPASSWORD } = process.env;
 
 //!Creo la base de datos
 // En este caso la base de datos se llamara "sequelize"
-/* const sequelize = new Sequelize(
+const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
- */
 
 /* Conexion con Railway para el deploy */
-const sequelize = new Sequelize(PGPASSWORD, {
+/* const sequelize = new Sequelize(PGPASSWORD, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+}); */
 
 const basename = path.basename(__filename);
 
