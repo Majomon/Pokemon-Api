1af0901cb16d4856243d7b7d997ba2a88bdd2559
Majomon <mauri.monzon91@gmail.com>
2023-07-14 16:10:26 -0300

    Update

--- src/app.js
+++ src/app.js
@@ -4,23 +4,22 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require("cors");
 
 require("./db.js");
 
 const server = express();
 
 server.name = "API";
-const corsOptions = {
-  origin: "https://pokemon-client-cyan.vercel.app",
-  credentials: true,
-};
+
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", corsOptions.origin);
-  res.header("Access-Control-Allow-Credentials", corsOptions.credentials);
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
