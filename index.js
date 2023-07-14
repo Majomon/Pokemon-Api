1ce927fb318bd8f56e6b05ad5a3327ac4bd9a45f
Majomon <mauri.monzon91@gmail.com>
2023-07-14 13:52:35 -0300

    Probando

--- index.js
+++ index.js
@@ -18,14 +18,13 @@
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 require("dotenv").config();
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 3001;
 
-
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
-  });
+/* conn.sync({ force: true }).then(() => { */
+server.listen(PORT, () => {
+  console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
 });
+/* }); */
--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, PGPASSWORD } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, PGDATABASE, PGPASSWORD } = process.env;
 
 //!Creo la base de datos
 // En este caso la base de datos se llamara "sequelize"
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${PGDATABASE}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
