48a94d1b7cb953d407c950f6efe0b4cdb6a1ee42
Majomon <mauri.monzon91@gmail.com>
2023-07-14 14:22:48 -0300

    Probando

--- src/db.js
+++ src/db.js
@@ -22,6 +22,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST, PGPASSWORD, POSTGRES_URL } = process.env;
 
 //! Vercel
 const sequelize = new Sequelize(POSTGRES_URL + "?sslmode=require", {
+  dialect: "postgres",
+  dialectModule: pg,
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
