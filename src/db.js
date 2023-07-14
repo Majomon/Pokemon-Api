3168f7176ac23dd339a1ff292eeffb49678bda33
Majomon <mauri.monzon91@gmail.com>
2023-07-14 14:18:07 -0300

    Probando

--- src/db.js
+++ src/db.js
@@ -2,26 +2,29 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, PGDATABASE, PGPASSWORD } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, PGPASSWORD, POSTGRES_URL } = process.env;
 
 //!Creo la base de datos
 // En este caso la base de datos se llamara "sequelize"
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${PGDATABASE}`,
+/* const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
   {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectModule: pg,
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);
+); */
 
 /* Conexion con Railway para el deploy */
 /* const sequelize = new Sequelize(PGPASSWORD, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 }); */
+
+//! Vercel
+const sequelize = new Sequelize(POSTGRES_URL + "?sslmode=require", {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
 
 const basename = path.basename(__filename);
 
